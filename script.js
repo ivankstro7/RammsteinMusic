917f220057a8b52b40893a25b84da696f4b1c9ce
ivankstro7 <ivankstro7@gmail.com>
2021-07-08 11:05:30 -0500

    js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function elementos(){
     main-container-progress = document.getElementById('main-container-progress');
     progress = document.getElementById('progress');
     icon-play = document.getElementById('iconPlay');
+    status = document.getElementsByClassName('play-icon')[0];
     repeat-icon = document.getElementsByClassName('repeat-icon')[0];
     song = document.getElementsByTagName('audio')[0];
     start = document.getElementsByClassName('start')[0];
@@ -13,7 +14,12 @@ function elementos(){
 function play(){
     if((pista.paused==false) && (pista.ended==false)){
         pista.paused();
-        icon-play.className="pause-icon play-icon";
+        icon-play.className="play-icon";
+    } else {
+        pista.play();
+        icon-play.className="play-icon";
+
+        load = setInterval(rep, 1)
     }
 }
 
