3c0160af326a09af559f0c6333b7d174275c2622
ivankstro7 <ivankstro7@gmail.com>
2021-07-08 10:50:42 -0500

    update/js

---
+++ script.js
@@ -0,0 +1,27 @@
+function elementos(){
+    main-container-progress = document.getElementById('main-container-progress');
+    progress = document.getElementById('progress');
+    icon-play = document.getElementById('iconPlay');
+    repeat-icon = document.getElementsByClassName('repeat-icon')[0];
+    song = document.getElementsByTagName('audio')[0];
+    start = document.getElementsByClassName('start')[0];
+    end = document.getElementsByClassName('end')[0];
+
+    iconPlay.addEventListener('click', play, false);
+}
+
+function play(){
+    if((pista.paused==false) && (pista.ended==false)){
+        pista.paused();
+    }
+}
+
+
+
+
+
+
+
+
+
+window.addEventListener('load',elementos,false);
